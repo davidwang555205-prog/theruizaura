a2747c90c33b6edfa5ba85ac4a2a84e5adbfa141
davidwang555205-prog <davidwang555205@gmail.com>
2026-08-27 19:36:20 +0800

    feat(video-script): expose V1 compiler

---
+++ src/video-script/index.ts
@@ -0,0 +1,13 @@
+export { compileSeedance25VideoScript } from "./compiler";
+export { buildFilmSpec, validateFilmSpec } from "./filmSpecs";
+export { VIDEO_CAMERA_LIBRARY, VIDEO_MOTION_LIBRARY } from "./libraries";
+export type {
+  FilmShot,
+  FilmSpec,
+  ProductProtectionSpec,
+  VideoDuration,
+  VideoReferenceMapping,
+  VideoSceneSpec,
+  VideoScriptInput,
+  VideoScriptResult,
+} from "./contracts";
